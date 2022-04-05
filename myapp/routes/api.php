32bc8d1bb18be5e3914a5abed13a798341fa2ddd
sajid9505 <sajid.mahmud9505@gmail.com>
2022-04-06 04:51:44 +0600

    get all article API working

--- myapp/routes/api.php
+++ myapp/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', "App\Http\Controllers\ArticleController@index");
+
+Route::get('articles/{id}', function($id){
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request){
+    return Article::create($request->all);
+});
+
+Route::put('articles/{id}', function(Request $request, $id){
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+});
+
+Route::delete('articles/{id}', function($id){
+    Article::find($id)->delete();
+
+    return 204;
+});
+
+
